37a90f3ad85f59d6300b17d5e66ca9b3ee6acadf
Siddharth Pawar <siddhartha.pawar17@gmail.com>
2020-05-03 19:49:19 +0530

    Rocket Movement Added

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    [SerializeField] float thrustSpeed = 1.0f;
+    [SerializeField] float rotateSpeed = 1.0f;
+
+    Rigidbody myRigidbody = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        // Returns true when Key is held down
+        if (Input.GetKey(KeyCode.Space))
+        {
+            myRigidbody.AddRelativeForce(Vector3.up * thrustSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward, rotateSpeed * Time.deltaTime, Space.World);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward, rotateSpeed * Time.deltaTime, Space.World);
+        }
+    }
+}
