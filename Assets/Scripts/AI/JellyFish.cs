baed99f9693d1ec150e6d113ce7ef422140863d5
Siddharth <siddhartha.pawar17@gmail.com>
2021-05-03 16:35:14 +0530

    Jelly Fish Physics Based Motion added

--- Assets/Scripts/AI/JellyFish.cs
+++ Assets/Scripts/AI/JellyFish.cs
@@ -5,34 +5,62 @@ using UnityEngine;
 namespace ProjectBoost.AI {
     public class JellyFish : MonoBehaviour
     {
-        [SerializeField] float movementSpeed = 10.0f;
+        [SerializeField] float accelerationMagnitude = 10.0f;
 
-        [SerializeField]
-        Vector3 movementDirection = new Vector3(1.0f, 1.0f, 0.0f);
+        Vector3 accelrationDirection = new Vector3(1.0f, 1.0f, 0.0f);
+
+        [SerializeField] float delayBetweenImpulses = 1.0f;
+
+        Coroutine PeriodicImpulseEnumarator = null;
 
         Rigidbody m_rigidbody;
+        Animator m_animator;
 
         private void Start()
         {
             m_rigidbody = GetComponent<Rigidbody>();
+            m_animator = GetComponentInChildren<Animator>();
+            
+            accelrationDirection.Normalize();
 
-            movementDirection.Normalize();
+            m_rigidbody.velocity = accelrationDirection * accelerationMagnitude;
 
-            m_rigidbody.velocity = movementDirection * movementSpeed;
+            PeriodicImpulseEnumarator = StartCoroutine(PeriodicImpulse());
+            
         }
 
         private void ChangeDirection(Collision collision)
         {
             Vector3 Normal = collision.contacts[0].normal;
+            accelrationDirection = Vector3.Reflect(accelrationDirection.normalized, Normal);
+            accelrationDirection.y = Mathf.Abs(accelrationDirection.y);
+            
+        }
 
-            movementDirection = Vector3.Reflect(movementDirection.normalized, Normal);
+        private void ApplyImpusle(float factor = 1.0f) {
+            m_animator.SetTrigger("Accelerate");
 
+            Vector3 amount = accelrationDirection * accelerationMagnitude * factor;
+            amount.y *= 2.5f;
+            m_rigidbody.velocity += amount;
         }
 
         private void OnCollisionEnter(Collision collision)
         {
+            StopCoroutine(PeriodicImpulseEnumarator);
             ChangeDirection(collision);
-            m_rigidbody.velocity = movementDirection * movementSpeed;
+            ApplyImpusle(1.2f);
+            
+            PeriodicImpulseEnumarator = StartCoroutine(PeriodicImpulse());
+
+        }
+
+        private IEnumerator PeriodicImpulse() {
+            while (true) {
+                yield return new WaitForSeconds(delayBetweenImpulses);
+                ApplyImpusle();
+                
+            }
         }
     }
 }
