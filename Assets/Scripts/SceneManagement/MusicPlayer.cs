817368e3d517f7e8fd3a5c1bc447a775b794429d
Siddharth <siddhartha.pawar17@gmail.com>
2021-07-26 21:46:57 +0530

    Music Player Added

---
+++ Assets/Scripts/SceneManagement/MusicPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProjectBoost.SceneManagement {
+    public class MusicPlayer : MonoBehaviour
+    {
+
+        void Awake() {
+            DontDestroyOnLoad(this);
+        }
+    }
+}
+
